a2989711ccb7425e8214d024f9ec19f11c64b0f7
Kang Namgyu <kangnamgyusch@gmail.com>
2020-11-21 03:59:03 +0900

    오류수정 후 실행되는 것을 확인함

--- algorithm/src/algorithm_subscriber.py
+++ algorithm/src/algorithm_subscriber.py
@@ -3,9 +3,10 @@ import rospy
 from common_msgs.msg import Mesgtype
 
 def callback(msg):
-    print(msg.Speed)
-    print(msg.Distance)
+    print "Time :", msg.timestamp.secs%100
+    print "Speed | x :", msg.Speed.x, "| y :", msg.Speed.y, "| z :", msg.Speed.z
+    print "Distance :", msg.Distance.data, "m"
 
 rospy.init_node('Algorithm')
-sub = rospy.Subscriber('Algorithm_msg', Mesgtype, callback)
+sub = rospy.Subscriber('Msgs_msg', Mesgtype, callback)
 rospy.spin()
--- sensor/src/sensor_publisher.py
+++ sensor/src/sensor_publisher.py
@@ -5,23 +5,23 @@ from geometry_msgs.msg import Vector3
 from common_msgs.msg import Mesgtype
 
 rospy.init_node('Sensor')
-pub = rospy.Publisher('Sensor_msg', Mesgtype, queue_size=1)
+pub = rospy.Publisher('Msgs_msg', Mesgtype, queue_size=1)
 msg = Mesgtype()
 rate = rospy.Rate(1)
 msg.Speed.x = 0
 msg.Speed.y = 0
 msg.Speed.z = 0
-msg.Distance = 0
+msg.Distance.data = 0
 
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
-    second = msg.timestamp.secs
     msg.Speed.x += 1
     msg.Speed.y += 1.5
     msg.Speed.z += 2
-    msg.Distance = msg.Distance + msg.Speed.x + msg.Speed.y
+    msg.Distance.data = msg.Distance.data + msg.Speed.x + msg.Speed.y
     pub.publish(msg)
 
-    print(msg.Speed)
-    print(msg.Distance)
+    print "Time :", msg.timestamp.secs%100
+    print "Speed | x :", msg.Speed.x, "| y :", msg.Speed.y, "| z :", msg.Speed.z
+    print "Distance :", msg.Distance.data, "m"
     rate.sleep()
