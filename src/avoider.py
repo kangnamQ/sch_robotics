056b32dadf5e3f56d57d62c91a89a9eb6b986d90
Kang <kangnamgyusch@gmail.com>
2020-12-05 22:11:50 +0900

    Modify avoider.py

--- src/avoider.py
+++ src/avoider.py
@@ -1,8 +1,9 @@
 #!/home/pi/.pyenv/versions/rospy3/bin/python
 
 import rospy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist         #move wheel
+from sensor_msgs.msg import LaserScan       #sacn
+#laserscan data -> Twist -> robot move
 
 class SelfDrive:
     def __init__(self, publisher):
@@ -12,21 +13,31 @@ class SelfDrive:
     def lds_callback(self, scan):
         # scan 분석 후 속도 결정
         # ...
-        print("scan[0]:", scan.ranges[0])
+        left_scan_30 = 0
+        for i in range(30):
+            # print(f"{i}_th angle : ", scan.ranges[i])
+            left_scan_30 += scan.ranges[i]
+        mean_left_scan_30 = left_scan_30 / 30
+        print("sum of left scan : ", left_scan_30)
+        print("mean of left scan : ", mean_left_scan_30)
         turtle_vel = Twist()
-         # 전진 속도 및 회전 속도 지정
-        if self.count < 100:
+
+        if mean_left_scan_30 < 0.3:
             turtle_vel.linear.x = 0.1
-            self.count += 1
+            turtle_vel.angular.z = -2.0
+            self.publisher.publish(turtle_vel)
+
         else:
-            turtle_vel.linear.x = 0.0
-        turtle_vel.angular.z = 0.0
-         # 속도 출력
-        self.publisher.publish(turtle_vel)
+            turtle_vel.linear.x = 0.15
+            turtle_vel.angular.z = 0.0
+            self.publisher.publish(turtle_vel)
+            # turtle_vel.angular.z = 1
+        # 속도 출력
+
 
 def main():
-    rospy.init_node('self_drive')
-    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.init_node('Final_Project')
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)     #It's already fixed.
     driver = SelfDrive(publisher)
     subscriber = rospy.Subscriber('scan', LaserScan,
                                   lambda scan: driver.lds_callback(scan))
