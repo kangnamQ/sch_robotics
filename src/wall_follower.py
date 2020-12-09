3f8b26b84993f336f597ff11da776e8ab6d17917
Kang <kangnamgyusch@gmail.com>
2020-12-09 16:58:33 +0900

    push origin

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -38,22 +38,28 @@ class SelfDrive:
         right = sum(scan_right) / len(scan_right)
 
         turtle_vel = Twist()
+        if front < 0.25 and status == 0:
+            turtle_vel.linear.x = 0.0
+            turtle_vel.angular.z = 2.5  # + => turn left / - => turn right
+            status = 1
+            self.publisher.publish(turtle_vel)
+            print("front wall! turn left!")
+        # 만약 앞에 무언가 나타났다면 다시 좌회전을합니다.
+        elif right < 0.25 and front < 0.25 and status == 1:
+            turtle_vel.linear.x = 0.0
+            turtle_vel.angular.z = 2.5  # + => turn left / - => turn right
+            self.publisher.publish(turtle_vel)
+            print("There's something in front of me! turn left!")
         #왼쪽으로 돌았을 경우 오른쪽에 벽이 있어야 하기 떄문에 벽을 탐지하면 타고 직진합니다. 이떄 정면에는 아무것도 없어야합니다.
-        if right < 0.25 and front > 0.25 and status == 1:
+        elif right < 0.25 and front > 0.25 and status == 1:
             turtle_vel.linear.x = 2.0
             turtle_vel.angular.z = 0.0  # + => turn left / - => turn right
             self.publisher.publish(turtle_vel)
             print("Follow the wall!")
-        # 만약 앞에 무언가 나타났다면 다시 좌회전을합니다.
-        elif right < 0.25 and front < 0.25 and status == 1:
-            turtle_vel.linear.x = 0.0
-            turtle_vel.angular.z = 2.0  # + => turn left / - => turn right
-            self.publisher.publish(turtle_vel)
-            print("There's something in front of me! turn left!")
         #만약 벽을따라 주행중인 상태인데 장애물떄문에 회피했다가 벽이없어진다면 (오른쪽으로 돌아서 따라가야할 때)
-        elif right > 0.45 and front > 0.45 and status == 1 and fright > 0.4:
+        elif right > 0.35 and front > 0.35 and status == 1 and fright > 0.5:
             turtle_vel.linear.x = 0.0
-            turtle_vel.angular.z = -2.0  # + => turn left / - => turn right
+            turtle_vel.angular.z = -2.5  # + => turn left / - => turn right
             self.publisher.publish(turtle_vel)
             print("No wall! turn turn right!")
         #만약에 벽을 따라 주행중인데 벽에서 점점 벌어진다면
@@ -69,12 +75,7 @@ class SelfDrive:
         #    self.publisher.publish(turtle_vel)
         #    print("Stick to the wall!")
         # 처음 벽을 찾는 부분에서 벽을 만나면 좌회전을합니다. 벽을 만나게 하기 위한 처음 조건입니다.
-        elif front < 0.25 and status == 0:
-            turtle_vel.linear.x = 0.0
-            turtle_vel.angular.z = 2.0  # + => turn left / - => turn right
-            status = 1
-            self.publisher.publish(turtle_vel)
-            print("front wall! turn left!")
+
         #기본적으로 직진을 합니다.
         else:
             turtle_vel.linear.x = 2.0
